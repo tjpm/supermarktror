e4a780808b7c6079f920b46186b3b3c9e19ca9d9
tj <tarek.jakobeit@haw-hamburg.de>
2018-10-29 23:47:52 +0100

    Initial commit

---
+++ app/controllers/menu_controller.rb
@@ -0,0 +1,17 @@
+class MenuController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+end
---
+++ app/controllers/statistics_controller.rb
@@ -0,0 +1,6 @@
+class StatisticsController < ApplicationController
+  def statistics
+    @title = 'Statistics Overview'
+    @content = 'Here are some statistics'
+  end
+end
